29b98a52c119f97d9da43d222ff03ef674a90175
Gleuton Dutra <gleuton.dutra@gmail.com>
2020-03-11 16:50:04 -0300

    docker

---
+++ nonconformities/database/factories/ModelFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+/** @var Factory $factory */
+
+use App\User;
+use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(User::class, static function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->email,
+    ];
+});
