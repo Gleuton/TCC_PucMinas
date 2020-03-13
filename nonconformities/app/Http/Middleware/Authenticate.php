f9b50481cc7651351f90f6785ad04b176a9faab3
Gleuton Dutra <gleuton.dutra@gmail.com>
2020-03-13 15:36:56 -0300

    nonconformities table - migration

--- nonconformities/app/Http/Middleware/Authenticate.php
+++ nonconformities/app/Http/Middleware/Authenticate.php
@@ -4,20 +4,22 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Contracts\Auth\Factory as Auth;
+use Illuminate\Http\Request;
 
 class Authenticate
 {
     /**
      * The authentication guard factory instance.
      *
-     * @var \Illuminate\Contracts\Auth\Factory
+     * @var Auth
      */
     protected $auth;
 
     /**
      * Create a new middleware instance.
      *
-     * @param  \Illuminate\Contracts\Auth\Factory  $auth
+     * @param Auth $auth
+     *
      * @return void
      */
     public function __construct(Auth $auth)
@@ -28,9 +30,9 @@ class Authenticate
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string|null  $guard
+     * @param  Request     $request
+     * @param Closure      $next
+     * @param  string|null $guard
      * @return mixed
      */
     public function handle($request, Closure $next, $guard = null)
---
+++ nonconformities/database/migrations/2020_03_13_182140_create_nonconformities_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNonconformitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create(
+            'nonconformities',
+            static function (Blueprint $table) {
+                $table->id();
+                $table->timestamps();
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('nonconformities');
+    }
+}
