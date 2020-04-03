6e101ec380055ac996a71ea7af28e6fae9de7a89
Gleuton Dutra <gleuton.dutra@gmail.com>
2020-04-03 18:41:58 -0300

    tests models nc, cn status and nc type

---
+++ schedules/tests/NcStatusTest.php
@@ -0,0 +1,57 @@
+<?php
+
+use App\Models\NcStatus;
+use App\Models\Nonconformity;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+
+class NcStatusTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function testInsertNcStatus(): void
+    {
+        $data = factory(NcStatus::class)->create();
+        $this->assertInstanceOf(Model::class, $data);
+    }
+
+    public function testSelectNcStatus(): void
+    {
+        $data = factory(NcStatus::class, 6)->create();
+        $this->assertInstanceOf(Collection::class, $data);
+        $this->assertCount(6, $data);
+    }
+
+    public function testSelectOneNcStatus(): void
+    {
+        $data = factory(NcStatus::class)->create();
+        $ncStatus = NcStatus::find($data->id);
+        $this->assertInstanceOf(Model::class, $ncStatus);
+    }
+
+    public function testDeleteNcStatus(): void
+    {
+        $data = factory(NcStatus::class)->create();
+        $ncStatus = NcStatus::find($data->id);
+        $this->assertInstanceOf(Model::class, $ncStatus);
+        $this->assertTrue($ncStatus->delete());
+    }
+
+    public function testUpdateNcStatus(): void
+    {
+        $update = ['status' => 'inativo'];
+        $data = factory(NcStatus::class)->create();
+        $ncStatus = NcStatus::find($data->id);
+        $this->assertInstanceOf(Model::class, $ncStatus);
+        $this->assertTrue($ncStatus->update($update));
+        $ncStatus = NcStatus::find($data->id)->toArray();
+        $this->assertEquals($ncStatus['status'], $update['status']);
+    }
+
+    public function testSeletNcByNcStatus(): void
+    {
+        $data = factory(NcStatus::class)->create();
+        $this->assertInstanceOf(Collection::class, $data->nonconformities);
+    }
+}
---
+++ schedules/tests/NonconformityTest.php
@@ -0,0 +1,62 @@
+<?php
+
+use App\Models\NcStatus;
+use App\Models\NcType;
+use App\Models\Nonconformity;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+
+class NonconformityTest extends TestCase
+{
+    use DatabaseMigrations;
+    public function testInsertNc(): void
+    {
+        $data = factory(Nonconformity::class)->create();
+        $this->assertInstanceOf(Model::class, $data);
+    }
+
+    public function testSelectNc(): void
+    {
+        $data = factory(Nonconformity::class, 6)->create();
+        $this->assertInstanceOf(Collection::class, $data);
+        $this->assertCount(6, $data);
+    }
+
+    public function testSelectOneNc(): void
+    {
+        $data = factory(Nonconformity::class)->create();
+        $nc = Nonconformity::find($data->id);
+        $this->assertInstanceOf(Model::class, $nc);
+    }
+
+    public function testDeleteNc(): void
+    {
+        $data = factory(Nonconformity::class)->create();
+        $nc = Nonconformity::find($data->id);
+        $this->assertInstanceOf(Model::class, $nc);
+        $this->assertTrue($nc->delete());
+    }
+
+    public function testUpdateNc(): void
+    {
+        $update = ['standard' => 'regra'];
+        $data = factory(Nonconformity::class)->create();
+        $nc = Nonconformity::find($data->id);
+        $this->assertInstanceOf(Model::class, $nc);
+        $this->assertTrue($nc->update($update));
+        $nc = Nonconformity::find($data->id)->toArray();
+        $this->assertEquals($nc['standard'], $update['standard']);
+    }
+
+    public function testSeletNcStatusByNc(): void
+    {
+        $nc = factory(Nonconformity::class)->create();
+        $this->assertInstanceOf(NcStatus::class, $nc->status);
+    }
+    public function testSeletNcTypeByNc(): void
+    {
+        $nc = factory(Nonconformity::class)->create();
+        $this->assertInstanceOf(NcType::class, $nc->type);
+    }
+}
