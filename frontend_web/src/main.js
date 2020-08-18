8fb9cfa651f571cea26fbb042465d30cfa5c6e4f
Gleuton Dutra <gleuton.dutra@gmail.com>
2020-08-17 21:27:01 -0300

    front structure

--- frontend_web/src/main.js
+++ frontend_web/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
 import router from './router'
 import store from './store'
+
+import './assets/css/app.scss'
 
 Vue.config.productionTip = false
 
