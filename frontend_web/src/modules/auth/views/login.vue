e07488c97c4c2553d6be1f0fa2897f48843e16e3
Gleuton Dutra <gleuton.dutra@gmail.com>
2020-08-18 13:56:34 -0300

    login form

--- frontend_web/src/modules/auth/views/login.vue
+++ frontend_web/src/modules/auth/views/login.vue
@@ -1,15 +1,48 @@
 <template>
-    <div>
-        <h1>Login</h1>
-    </div>
+  <div id="login">
+    <form @submit.prevent="submit()">
+      <div class="login-page">
+        <div class="card">
+          <div class="card-header">Login</div>
+          <div class="card-body">
+            <div class="form-group">
+              <input required type="email" v-model="form.email" class="form-control" placeholder="E-mail" />
+            </div>
+            <div class="form-group">
+              <input required type="password" v-model="form.password" class="form-control" placeholder="Senha" />
+            </div>
+            <button class="btn btn-primary btn-lg btn-block">Entrar</button>
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data: () => ({
+    form: {
+      email: '',
+      password: ''
+    }
+  }),
+  methods: {
+    submit () {
+      console.log(this.form)
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped lang="scss">
+.login-page {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.card {
+  width: 35%;
+}
 </style>
