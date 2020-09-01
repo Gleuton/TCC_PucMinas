3094bef14e454f0a45a168f2dc8128f5f9b429b2
Gleuton Dutra <gleuton.dutra@gmail.com>
2020-08-31 22:42:32 -0300

    validate form cad user

--- frontend_web/src/views/user/CadForm.vue
+++ frontend_web/src/views/user/CadForm.vue
@@ -11,9 +11,12 @@
             id="name"
             v-model="form.name"
             type="text"
-            required
+            @keyup="name_validate()"
             placeholder="Nome do Usuário"
           ></b-form-input>
+          <small class="input-error" v-if="!validation_form.name.valid">
+            {{ validation_form.name.message }}
+          </small>
         </b-form-group>
 
         <b-form-group
@@ -25,44 +28,62 @@
             id="login"
             v-model="form.login"
             type="email"
-            required
+            @keyup="email_validate()"
             placeholder="E-mail do Usuário"
           ></b-form-input>
+          <small class="input-error" v-if="!validation_form.login.valid">
+            {{ validation_form.login.message }}
+          </small>
         </b-form-group>
         <b-form-group
           id="input-group-type"
           label="Tipo de Usuário:"
           label-for="userTypes"
         >
-          <b-form-select id="userTypes" v-model="form.user_type_id" :options="optionsUserType()"></b-form-select>
+          <b-form-select
+          id="userTypes"
+          v-model="form.user_type_id"
+          :options="optionsUserType()"
+          @change="type_validate()"
+          ></b-form-select>
+          <small class="input-error" v-if="!validation_form.user_type_id.valid">
+            {{ validation_form.user_type_id.message }}
+          </small>
         </b-form-group>
 
         <b-form-group
           id="input-group-password"
           label="Senha:"
+          @keyup="password_validate"
           label-for="password"
         >
           <b-form-input
             id="password"
             v-model="form.password"
             type="password"
-            required
             placeholder="Senha"
+            @keyup="password_validate()"
           ></b-form-input>
+          <small class="input-error" v-if="!validation_form.password.valid">
+            {{ validation_form.password.message }}
+          </small>
         </b-form-group>
 
         <b-form-group
           id="input-group-password-confirmation"
-          label="Senha:"
+          label="Confirme a senha:"
           label-for="confirmation"
         >
           <b-form-input
             id="confirmation"
             v-model="form.password_confirmation"
             type="password"
-            required
+            @keyup="confim_pwd_validate()"
             placeholder="Confirme a Senha"
           ></b-form-input>
+          <small class="input-error" v-if="!validation_form.password_confirmation.valid">
+            {{ validation_form.password_confirmation.message }}
+          </small>
         </b-form-group>
 
         <div class="button-box">
@@ -70,13 +91,6 @@
           <b-button type="submit" variant="success">Salvar</b-button>
         </div>
       </b-form>
-      <div v-if="errors" class="box-errors">
-        <div v-for="(v, k) in errors" :key="k">
-          <p v-for="error in v" :key="error" class="text-sm">
-            {{ error }}
-          </p>
-        </div>
-      </div>
     </b-card>
   </div>
 </template>
@@ -89,13 +103,34 @@ export default {
   data () {
     return {
       form: {
-        nome: '',
+        name: '',
         login: '',
         user_type_id: '',
         password: '',
         password_confirmation: ''
       },
-      errors: null
+      validation_form: {
+        name: {
+          valid: true,
+          message: ''
+        },
+        login: {
+          valid: true,
+          message: ''
+        },
+        user_type_id: {
+          valid: true,
+          message: ''
+        },
+        password: {
+          valid: true,
+          message: ''
+        },
+        password_confirmation: {
+          valid: true,
+          message: ''
+        }
+      }
     }
   },
   mounted () {
@@ -113,14 +148,16 @@ export default {
     ]),
     async onSubmit (evt) {
       evt.preventDefault()
-      try {
-        await this.ActionAddUser(
-          JSON.stringify(this.form)
-        )
-        this.$toastr.s('Sucesso ao Cadastrar')
-        this.back()
-      } catch (error) {
-        this.errors = error.data
+      if (this.validate()) {
+        try {
+          await this.ActionAddUser(
+            JSON.stringify(this.form)
+          )
+          this.$toastr.s('Sucesso ao Cadastrar')
+          this.back()
+        } catch (error) {
+          this.$toastr.e('Erro ao Cadastrar')
+        }
       }
     },
     back () {
@@ -138,6 +175,67 @@ export default {
         if (a.text < b.text) return -1
         return 0
       })
+    },
+    validate () {
+      const name = this.name_validate()
+      const email = this.email_validate()
+      const type = this.type_validate()
+      const password = this.password_validate()
+      const confirm = this.confim_pwd_validate()
+
+      return name && email && type && password && confirm
+    },
+    name_validate () {
+      this.validation_form.name.valid = true
+      if (this.form.name.length <= 0) {
+        this.validation_form.name.valid = false
+        this.validation_form.name.message = 'Este campo é obrigatório.'
+      }
+      if (this.form.name.length >= 255) {
+        this.validation_form.name.valid = false
+        this.validation_form.name.message = 'Este campo deve ser menor que 255 caracteres.'
+      }
+      return this.validation_form.name.valid
+    },
+    email_validate () {
+      this.validation_form.login.valid = true
+      if (this.form.login.length <= 0) {
+        this.validation_form.login.valid = false
+        this.validation_form.login.message = 'Este campo é obrigatório.'
+      }
+      if (this.form.login.length >= 255) {
+        this.validation_form.login.valid = false
+        this.validation_form.login.message = 'Este campo deve ser menor que 255 caracteres.'
+      }
+      return this.validation_form.login.valid
+    },
+    type_validate () {
+      this.validation_form.user_type_id.valid = true
+      if (this.form.user_type_id === '') {
+        this.validation_form.user_type_id.valid = false
+        this.validation_form.user_type_id.message = 'Este campo é obrigatório.'
+      }
+      return this.validation_form.user_type_id.valid
+    },
+    password_validate () {
+      this.validation_form.password.valid = true
+      if (this.form.password.length <= 0) {
+        this.validation_form.password.valid = false
+        this.validation_form.password.message = 'Este campo é obrigatório.'
+      }
+      if (this.form.password.length >= 255) {
+        this.validation_form.password.valid = false
+        this.validation_form.password.message = 'Este campo deve ser menor que 255 caracteres.'
+      }
+      return this.validation_form.password.valid
+    },
+    confim_pwd_validate () {
+      this.validation_form.password_confirmation.valid = true
+      if (this.form.password !== this.form.password_confirmation) {
+        this.validation_form.password_confirmation.valid = false
+        this.validation_form.password_confirmation.message = 'A confirmação da senha não corresponde.'
+      }
+      return this.validation_form.password_confirmation.valid
     }
   }
 }
